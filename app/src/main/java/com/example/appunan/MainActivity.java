2d1f62e80d3815429082184c24234c0d80b198b1
Amélina Gancel <amelinag99@gmail.com>
2020-11-06 16:13:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/appunan/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.appunan;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+    private MapView map; //creation de la map
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Configuration.getInstance().load(getApplicationContext(),
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+        setContentView(R.layout.activity_main);
+        map= findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK); //render
+        map.setBuiltInZoomControls(true);  //pour le zoom
+        GeoPoint startPoint = new GeoPoint(48.41090774536133,-4.491884231567383); //Position de depart
+        IMapController mapController = map.getController();
+        mapController.setZoom(18.0);  //definir le zoom
+        mapController.setCenter(startPoint);
+
+        ArrayList<OverlayItem> items = new ArrayList<>();  //creation des pastilles
+        OverlayItem home = new OverlayItem("Secours Populaire Français","Association humanitaire, d'entraide, sociale",new GeoPoint(48.41090774536133,-4.491884231567383));  //creation de la pastille "home"
+        //Il y a le nom et la position de la pastille
+        Drawable m = home.getMarker(0);  //forme de la pastille (deja existante dans la librairie)
+        items.add(home); //ajout de la pastille home dans la liste des pastilles
+        items.add(new OverlayItem("ENIB", "Ecole National des Ingénieurs de Brest", new GeoPoint(48.36085891723633, -4.567451477050781)));  //creation d'une autre pastille en version plus compact
+
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getApplicationContext(),  //associer les pastilles avec la map
+                items, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {   //reaction au clic
+            @Override
+            public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                return true;
+            }
+
+            @Override
+            public boolean onItemLongPress(int index, OverlayItem item) {
+                return false;
+            }
+        });
+
+        mOverlay.setFocusItemsOnTap(true);  // clique sur la pastille
+        map.getOverlays().add(mOverlay);
+
+    }
+    @Override
+    public void onPause() {   //mise en pause de l'activité
+        super.onPause();
+        map.onPause();  // map mise en pause
+    }
+    @Override
+    public void onResume() {   //mise en route de l'activité
+        super.onResume();
+        map.onResume();
+    }
+
+
+}
